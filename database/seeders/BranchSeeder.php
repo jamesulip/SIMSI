05af8108cb8eb82e98f4d181ac0c0a92a10d5ff5
jamesulip <ulip.james@gmail.com>
2023-02-16 10:35:56 +0800

    hot fix

--- database/seeders/BranchSeeder.php
+++ database/seeders/BranchSeeder.php
@@ -17,6 +17,58 @@ class BranchSeeder extends Seeder
         //
         // Truncate
         \App\Models\Branch::truncate();
-        \App\Models\Branch::factory()->count(10)->create();
+        // \App\Models\Branch::factory()->count(10)->create();
+
+        // MAIN BRANCH
+        // Units 500 & 501 L & S Bldg., 1414 Roxas Blvd. cor. Sta. Monica St., Ermita, Manila, Philippines
+        //Telephone Nos. : (632) 523-7112; 523-4472
+        // Fax No.: (632) 525-1198
+        // email shaso2010@yahoo.com
+        // Website:  www.shasointernational.com
+
+
+
+        //
+        //         CAGAYAN DE ORO BRANCH
+        // Unit 2A, 2nd Floor, 52nd Business Center MIRDC Building Justo Gaerlan St., Cagayan De Oro City
+        // Telefax: 088-850-0039
+        // Email address: shasocagayan2014@gmail.com
+
+        //
+
+//         DAVAO BRANCH
+    // 2ND FLOOR, DOOR 2, BLUE BLDG., RIZAL EXTENSION, DAVAO CITY
+    // Telefax: 082-222-1612
+    // Email address: shaso.davao@gmail.com
+
+
+        $branchs = [
+            [
+                'name' => 'Main Branch',
+                'address' => 'Units 500 & 501 L & S Bldg., 1414 Roxas Blvd. cor. Sta. Monica St., Ermita, Manila, Philippines',
+                'phone' => ['(632) 523-7112', '(632) 523-4472'],
+                'email' => ['
+                shaso2010@yahoo.com
+            '],
+                'website' => '/',
+            ],
+            [
+                'name' => 'Cagayan De Oro Branch',
+                'address' => 'Unit 2A, 2nd Floor, 52nd Business Center MIRDC Building Justo Gaerlan St., Cagayan De Oro City',
+                'phone' => ['(088) 850-0039'],
+                'email' => ['shasocagayan2014@gmail.com'],
+                'website' => '/',
+            ],
+            [
+                'name' => 'Davao Branch',
+                'address' => '2ND FLOOR, DOOR 2, BLUE BLDG., RIZAL EXTENSION, DAVAO CITY',
+                'phone' => ['(082) 222-1612'],
+                'email' => ['shaso.davao@gmailcom'],
+                'website' => '/',
+            ]
+        ];
+       foreach ($branchs as $key => $branch) {
+           \App\Models\Branch::create($branch);
+       }
     }
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,24 +15,7 @@
 
         @inertiaHead
 
-        <script>
 
-function initMap() {
-  const myLatLng = { lat: -25.363, lng: 131.044 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: myLatLng,
-  });
-
-  new google.maps.Marker({
-    position: myLatLng,
-    map,
-    title: "Hello World!",
-  });
-}
-
-window.initMap = initMap;
-        </script>
     </head>
     <body class="font-sans antialiased">
         @inertia
